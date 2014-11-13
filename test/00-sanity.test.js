9d0316e2174cfd2029621c8d4967401b237eaf05
just-paja <pavel@proboston.net>
2014-11-13 14:48:15 +0100

    Fix for pwf.js-1

--- test/00-sanity.test.js
+++ test/00-sanity.test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 pwf = require('pwf.js')
 
 describe('sanity', function() {
-	it('tests only sanity of js code', function() {
+	it('module usability', function() {
 		var mod = null;
 
 		assert.doesNotThrow(function() {
@@ -10,7 +10,6 @@ describe('sanity', function() {
 		}, 'An error was thrown during mod inclusion.');
 
 		assert.notEqual(null, mod, 'mod was not loaded successfuly. Check if it\'s being exported properly.');
-		console.log(mod);
 		assert.equal(pwf.status('accents'), true, 'Module was not registered successfully');
 		pwf.test_sane = true;
 	});
